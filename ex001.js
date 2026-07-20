cecf03775ff97d03c9ea5ac94ef389d31f32a704
Xvr.Erik <ErickXavier254@gmail.com>
2026-07-20 16:11:34 +0000

    adiciona lógica de loop e exibição de números no arquivo ex001.js

--- ex001.js
+++ ex001.js
@@ -1,1 +1,22 @@
-console.log("ola, mundo!")
+
+let val = 1
+do{
+    console.log(`numero: ${val}`)
+    val += 1
+}while(val <= 8)
+
+
+const valor = 12
+let cont = 1
+console.log(`valores entre 1 e ${valor}`)
+do{
+    console.log(cont)
+    cont +=1
+}while (cont <= valor)
+
+let cont1 = 1
+do{
+    console.log(cont + cont)
+    cont +=1
+}while(cont <=10)
+
