f2312980638a829617bba0de6b4a18d2fee5f6ee
Xvr.Erik <ErickXavier254@gmail.com>
2026-07-22 17:15:40 +0000

    adiciona o arquivo ex006.js com a implementação de exercícios de manipulação de arrays

---
+++ ex006.js
@@ -0,0 +1,28 @@
+let arr = [3, 5, 7, 1, 9, 2]
+arr.push(3)
+arr[4] = 8
+arr.sort()
+console.log(arr)
+const elemento = arr.indexOf(5)
+console.log(`o index do valor desejado e ${elemento}`)
+//exercicio 2
+console.log("exercicio2")
+let arr2 = [44, 55, 45, 8, 12, 9]
+arr.push(2)
+arr.push(4)
+arr[5] = 66
+arr.sort()
+console.log(arr)
+const elemento2 = arr.indexOf(66)
+console.log(`o indice do valor desejado e ${elemento2}`)
+//exercicio3
+console.log("exercicio3")
+let arrtexto = ['ola', 'xvr.erik', 'notebook']
+arr[0] = 'tudo bem'
+arr.push('vscode')
+arr.sort()
+const elemento3 = arr.indexOf("vscode")
+console.log(`o array e: ${arrtexto}`)
+console.log("em forma de array")
+console.log(arrtexto)
+console.log(`e o index o do valor escolhido e: ${elemento3}`)
