93ce858531780ab19e1df21559b01c318b7659c3
Xvr.Erik <ErickXavier254@gmail.com>
2026-07-22 18:03:31 +0000

    adiciona o arquivo ex007.js com a implementação de funções matemáticas e manipulação de números

---
+++ ex007.js
@@ -0,0 +1,31 @@
+function parimpar(n) {
+    if (n %2 ==0){
+        return 'par'
+    }else{
+        return 'impar'
+    }
+}
+function soma(n1=0, n2=0) {
+    return n1 + n2
+}
+//fazendo a funcao dentro da variavel
+let mult = function (n1, n2) {
+    return n1 * n2
+}
+function fatorial(n){
+    let fat = 1
+    for (let con = n;con>1;con -=1){
+        fat *= con
+    }
+    return fat
+}
+let multiplicar = mult(4, 7)
+let somar = soma(9, 6)
+let res = parimpar(5)
+let fatorialres = fatorial(5)
+
+console.log(res)
+console.log(somar)
+console.log(multiplicar)
+console.log(fatorialres)
+
