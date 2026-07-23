1bf18e952f1f2d00c9ddc2fb53a841068c8d1058
Xvr.Erik <ErickXavier254@gmail.com>
2026-07-23 23:37:06 +0000

    adicionei o ex008.js para a utilizaçao de objetos

---
+++ ex008.js
@@ -0,0 +1,3 @@
+let users = {nome:"xvr.erik", idade:"15", area:"analize e desenvolvimento de sistemas"}
+console.log(users)
+console.log(`o usuario ${users.nome} tem idade ${users.idade} na area ${users.area}`)
